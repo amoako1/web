f56c0cc73826922fe5e1250a9052cf329a9aa577
amoako1 <thomasamoakobaoteng@gmail.com>
2019-07-18 16:05:19 +0100

    small change

--- myapp.py
+++ myapp.py
@@ -3,15 +3,6 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
  
-@app.route('/')
-def index():
-    return 'Hello world'
-
-
-@app.route('/foo/<name>')
-def foo(name):
-    return render_template('foo.html', to=name)
-
 
 @app.route('/index')
 def ind():
